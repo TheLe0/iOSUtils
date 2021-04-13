c504fd1d7a3b770d13a5671dc9759af0487bde38
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-04-13 08:59:35 -0300

    Cache util class

---
+++ src/DataStorage/Cache.swift
@@ -0,0 +1,122 @@
+
+import Dispatch
+
+public final class Cache<T>: CustomStringConvertible {
+    private var _value: T?
+    private let queue: DispatchQueue
+    
+    public init(_ value: T? = nil) {
+        self._value = value
+        self.queue = DispatchQueue(label: "Cache", qos: .userInteractive)
+    }
+    
+    public static var empty: Cache<T> {
+        Cache()
+    }
+    
+    public var value: T? {
+        get {
+            queue.sync{ _value }
+        }
+        set {
+            queue.sync{ _value = newValue }
+        }
+    }
+    
+    public var hasValue: Bool {
+        value != nil
+    }
+    
+    public func useCacheOrSet(_ initializer: () -> T) -> T {
+        let cache = queue.sync { _value }
+        if let value = cache {
+            return value
+        }
+        
+        let value = initializer()
+        
+        queue.sync {
+            if _value == nil {
+                _value = value
+            }
+        }
+        
+        return value
+    }
+    
+    public func clear() {
+        queue.sync {
+            _value = nil
+        }
+    }
+    
+    public func copy() -> Cache<T> {
+        queue.sync {
+            Cache(_value)
+        }
+    }
+    
+    public var description: String {
+        queue.sync {
+            "Cache(\(value.map{ "\($0)" } ?? "-"))"
+        }
+    }
+}
+
+public final class CacheDictionary<K, T>: CustomStringConvertible where K: Hashable {
+    private var dictionary: [K : T]
+    private let queue: DispatchQueue
+
+    public init(_ dictionary: [K : T] = [:]) {
+        self.dictionary = dictionary
+        self.queue = DispatchQueue(label: "CacheDictionary", qos: .userInteractive)
+    }
+    
+    public static var empty: CacheDictionary<K, T> {
+        CacheDictionary()
+    }
+    
+    public subscript (key: K) -> T? {
+        get {
+            queue.sync { dictionary[key] }
+        }
+        set {
+            queue.sync { dictionary[key] = newValue }
+        }
+    }
+    
+    public func useCacheOrSet(key: K, _ initializer: () -> T) -> T {
+        let cache = queue.sync { dictionary[key] }
+        if let value = cache {
+            return value
+        }
+        
+        let value = initializer()
+        
+        queue.sync {
+            if dictionary[key] == nil {
+                dictionary[key] = value
+            }
+        }
+        
+        return value
+    }
+
+    public func remove(key: K) {
+        queue.sync {
+            let _ = dictionary.removeValue(forKey: key)
+        }
+    }
+    
+    public func clear() {
+        queue.sync {
+            self.dictionary = [:]
+        }
+    }
+    
+    public var description: String {
+        queue.sync {
+            "Cache(\(dictionary))"
+        }
+    }
+}
