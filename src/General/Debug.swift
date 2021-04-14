aee94ff8554199dac3291bd0d0bef86ae4d9676d
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-04-14 07:45:21 -0300

    Debug

---
+++ src/General/Debug.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public class Debug {
+    private static let precision = 1000.0
+    
+    public static func measure<T>(_ label: String? = nil, _ f: () -> T) -> T {
+        if let label = label {
+            print("Start: \(label)")
+        }
+        
+        let date = Date()
+        
+        defer {
+            let intv = -date.timeIntervalSinceNow
+            let time: String
+            if intv < 1 {
+                time = "\(round(intv * precision * 1000) / precision) msec."
+            } else {
+                time = "\(round(intv * precision) / precision) sec."
+            }
+            if let label = label {
+                print("End: \(label), \(time)")
+            } else {
+                print(time)
+            }
+        }
+        
+        return f()
+    }
+}
